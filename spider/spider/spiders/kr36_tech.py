b4eeb0c8cdaf3cd7320e3ac153afd4858b31f192
caoyu <ricardocao.biker@gmail.com>
2019-09-14 22:00:10 +0800

    update

--- spider/spider/spiders/kr36_tech.py
+++ spider/spider/spiders/kr36_tech.py
@@ -1,6 +1,13 @@
 import scrapy
+import time
 from spider.items import TechItem 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
 
 class Kr36Tech(scrapy.Spider):
     name = 'kr36_tech'
@@ -8,11 +15,13 @@ class Kr36Tech(scrapy.Spider):
     start_urls = ['https://36kr.com/information/technology']
 
     def parse(self, response):
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(chrome_options=chrome_options)
         driver.get(self.start_urls[0])
         res = driver.execute_script('return window.initialState')
         news_list = res['information']['informationList']
+        time_now = int(time.time() * 1000)
         for newsItem in news_list:
+            time_pub = int(time.mktime(time.strptime(newsItem['published_at'], '%Y-%m-%d %H:%M:%S'))) * 1000
             item = TechItem()
             item['pub_time'] = newsItem['published_at']
             item['title'] = newsItem['title']
@@ -26,4 +35,4 @@ class Kr36Tech(scrapy.Spider):
             driver.get('https://36kr.com/p/' + str(newsItem['entity_id']))
             news_content_page = driver.execute_script('return window.initialState')
             item['news_content'] = news_content_page['articleDetail']['articleDetailData']['data']['content']
-            yield item
+            yield item 
