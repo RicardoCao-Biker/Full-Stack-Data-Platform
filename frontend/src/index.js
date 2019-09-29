3437a945acaa71a4d5ccefd3ae1c7f13efe18b53
RicardoCao <ricardocao.biker@gmail.com>
2019-09-29 18:12:40 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 import './index.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.less';
+import RouteMap from './router/routeMap';
 
-import RouteMap from './router/routeMap'
-
+React.Component.prototype.axios = axios;
 ReactDOM.render(<RouteMap/>, document.getElementById('root'));
 
