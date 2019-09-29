369f6ad8e95f647347672f71d3a7839aed50ae5b
RicardoCao <ricardocao.biker@gmail.com>
2019-09-29 18:16:07 +0800

    Update routeMap.jsx

--- frontend/src/router/routeMap.jsx
+++ frontend/src/router/routeMap.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import Login from '../page/Login'
-import Container from '../page/Container'
-import NoFound from '../page/NoFound'
+import Login from '../page/Login';
+import Container from '../page/Container';
+import NoFound from '../page/NoFound';
 
-import routeInfo from '../utils/routeInfo'
+import MusicIndex from '../page/Music/Index';
+import InternationalNews from '../page/News/World';
+import ChinaNews from '../page/News/China';
+
+import routeInfo from '../utils/routeInfo';
 
 const components = {
-
+    MusicIndex,
+    InternationalNews,
+    ChinaNews
 };
 
 class RouterMap extends React.Component {
@@ -20,8 +26,23 @@ class RouterMap extends React.Component {
           <Route path='/' render={() => (
             <Container>
               <Switch>
-                <Redirect exact path="/" to="/news" />
-                
+                <Redirect exact path="/" to="/music/index" />
+                {
+                    Object.entries(routeInfo).map(([routeTitle, routeItem]) => {
+                        if (routeItem.child) {
+                            return routeItem.child.map(childRoute => {
+                                return (
+                                    <Route path={childRoute.path} component={components[childRoute.component]} key={childRoute.path}/>
+                                );
+                            })
+                        }
+                        else {
+                            return (
+                                <Route path={routeItem.path} component={components[routeItem.component]} key={routeItem.path}/>
+                            );
+                        }
+                    })
+                }
                 <Route component={NoFound} />
               </Switch>
             </Container>
